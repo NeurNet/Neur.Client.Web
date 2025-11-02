00a8c184d0c324b04500c852a248e4a7df812cd1
Kirill Siukhin <kirillsiukhin@proton.me>
2025-11-02 16:15:10 +0500

    Add import alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
 
 function App() {
   return (
