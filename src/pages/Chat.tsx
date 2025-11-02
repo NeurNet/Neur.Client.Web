fee222a663d9fcad364197064ad427d35f69a5d9
Kirill Siukhin <kirillsiukhin@proton.me>
2025-11-03 02:47:12 +0500

    Add structure and styles for MVP

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,5 +1,21 @@
+import { useState, type FormEvent } from 'react';
+import classes from './Chat.module.scss';
+
 function Chat() {
-  return <div>Chat page</div>;
+  const [prompt, setPrompt] = useState('');
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className={classes.form}>
+        <input type="text" placeholder="Prompt" onChange={(e) => setPrompt(e.target.value)} />
+        <input type="submit" value="Generate" />
+      </form>
+    </div>
+  );
 }
 
 export default Chat;
--- src/pages/ModelList.tsx
+++ src/pages/ModelList.tsx
@@ -1,9 +1,23 @@
-import { Link } from 'react-router-dom';
+import classes from './ModelList.module.scss';
 
 function ModelList() {
   return (
-    <div>
-      You&apos;re not logged in. <Link to="/login">Log in</Link>
+    <div className={classes.wrapper}>
+      <h1>Выберите модель:</h1>
+      <div className={classes.models}>
+        <div className={classes.model}>
+          <h2>LLaMa 8B</h2>
+          <p>Text model v1.2</p>
+        </div>
+        <div className={classes.model}>
+          <h2>Midjourney XL</h2>
+          <p>Image model v1.5</p>
+        </div>
+        <div className={classes.model}>
+          <h2>Codex</h2>
+          <p>Code model v1</p>
+        </div>
+      </div>
     </div>
   );
 }
