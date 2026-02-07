90ab1e2c5dabbf2ba190766825a68851aa28431c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-02-07 16:46:15 +0500

    chore: import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
   plugins: [
     react({
       babel: {
@@ -10,4 +16,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+});
