9bb031ed6b99a87628cd575da396e24d18b6d8c0
Kirill Siukhin <kirillsiukhin@proton.me>
2026-02-07 05:31:09 +0500

    chore: add necessary libs, initialize router and font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import '@fontsource-variable/montserrat';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { router } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
