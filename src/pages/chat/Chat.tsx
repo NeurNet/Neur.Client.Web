f0f4c9ae6abe6e2eecf380bcb96780b5fc71d7b1
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-03 19:35:57 +0500

    feat: display user messages as they are sent

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,40 +1,52 @@
 import { useState } from 'react';
 import { useParams } from 'react-router';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { SendHorizonal } from 'lucide-react';
-import { fetchChat } from '@/api/chat';
-import { sendMessage } from '@/api/message';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { ErrorMessage } from '@/components/error-message';
 import { Loader } from '@/components/loader';
+import { ErrorMessage } from '@/components/error-message';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Messages } from './Messages';
+import { fetchChat, type Chat, type ChatWithMessages } from '@/api/chat';
+import { sendMessage } from '@/api/message';
 import classes from './Chat.module.css';
 
 export function Chat() {
   const { chatId } = useParams();
-
   const [message, setMessage] = useState('');
 
+  const queryClient = useQueryClient();
   const { data, isPending, error } = useQuery({
     queryKey: ['chats', chatId],
     queryFn: () => fetchChat(chatId || ''),
-  });
-
-  const queryClient = useQueryClient();
-  const mutation = useMutation({
-    mutationFn: sendMessage,
-    onSuccess: async () => {
-      setMessage('');
-      await queryClient.invalidateQueries({ queryKey: ['chats', chatId] });
-    },
   });
 
   const sendHandler = async (e: React.SubmitEvent) => {
     e.preventDefault();
     if (!chatId) return;
 
-    mutation.mutate({ chatId, prompt: message });
+    queryClient.setQueryData(
+      ['chats', chatId],
+      (chat: ChatWithMessages): ChatWithMessages => ({
+        ...chat,
+        messages: [
+          ...chat.messages,
+          {
+            id: crypto.randomUUID(),
+            chat_id: chatId,
+            created_at: new Date().toISOString(),
+            content: message,
+            role: 'user',
+          },
+        ],
+      }),
+    );
+
+    console.log('Sending generation request...');
+    const stream = await sendMessage({ chatId, prompt: message });
+
+    setMessage('');
+    console.log(stream);
   };
 
   if (isPending) return <Loader />;
@@ -56,12 +68,10 @@ export function Chat() {
           onChange={(e) => setMessage(e.target.value)}
           required
         />
-        <Button type="submit" variant="ghost" size="icon" disabled={mutation.isPending}>
+        <Button type="submit" variant="ghost" size="icon">
           <SendHorizonal size={20} />
         </Button>
       </form>
-
-      {mutation.error && <span>{mutation.error.message}</span>}
     </div>
   );
 }
