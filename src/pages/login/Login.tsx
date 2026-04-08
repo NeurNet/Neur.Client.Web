4867af146314949c325f0fd351b964e516c22c8e
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-08 17:38:57 +0500

    feat: start button redesign

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/redesign-button';
 import { Input } from '@/components/ui/input';
 import { requestLogin } from '@/api/user';
 import classes from './Login.module.css';
@@ -68,7 +68,7 @@ export function Login() {
               required
             />
 
-            <Button type="submit" showLoader={isPending}>
+            <Button type="submit">
               Войти
             </Button>
 
