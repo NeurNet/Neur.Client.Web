00470f39173730b3aadc41c50ae3e665f5ead51e
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-14 04:21:08 +0500

    feat: basic chat page layout

--- src/pages/chat/ui/chat.tsx
+++ src/pages/chat/ui/chat.tsx
@@ -2,6 +2,7 @@ import classes from './chat.module.css';
 import { useParams } from 'react-router';
 import { useQuery } from '@tanstack/react-query';
 import { ChatApi } from '@/entities/chat';
+import { ChatInput } from '@/shared/ui/chat-input';
 
 export function Chat() {
   const { chatId } = useParams();
@@ -14,9 +15,18 @@ export function Chat() {
   if (!chat.data) return null;
 
   return (
-    <div>
-      <h1>{chat.data.model_name}</h1>
-      <span>{chat.data.model}</span>
+    <div className={classes.wrapper}>
+      <div className={classes.header}>
+        <h1 className={classes.modelName}>{chat.data.model_name}</h1>
+        <span className={classes.model}>{chat.data.model}</span>
+      </div>
+
+      <div className={classes.chat}></div>
+
+      <ChatInput onSend={() => console.log()} />
+      <span className={classes.disclaimer}>
+        NeurNet может допускать ошибки. Проверяйте важную информацию.
+      </span>
     </div>
   );
 }
