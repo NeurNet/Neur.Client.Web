d651752de3ab43dc344e74bc1d937feade05ad29
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-18 01:19:16 +0500

    feat: overview page

--- src/pages/admin/ui/overview.tsx
+++ src/pages/admin/ui/overview.tsx
@@ -1,3 +1,28 @@
+import { BrainCircuit, Globe, Users } from 'lucide-react';
+import { OverviewCard } from './overview-card';
+import classes from './overview.module.css';
+
 export function Overview() {
-  return <span>Overview Page</span>;
+  return (
+    <div className={classes.cards}>
+      <OverviewCard
+        icon={<Users size={40} strokeWidth={2.5} />}
+        title="87"
+        description="пользователей"
+        to="/admin/users"
+      />
+      <OverviewCard
+        icon={<Globe size={40} strokeWidth={2.5} />}
+        title="3 247"
+        description="запросов всего"
+        to="/admin/requests"
+      />
+      <OverviewCard
+        icon={<BrainCircuit size={40} strokeWidth={2.5} />}
+        title="8"
+        description="моделей"
+        to="/admin/models"
+      />
+    </div>
+  );
 }
