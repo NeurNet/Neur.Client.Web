5c406212f14e5e29ab6e425883108889324d3034
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-24 23:51:40 +0500

    feat: add filters and search

--- src/pages/admin/users/ui/users.tsx
+++ src/pages/admin/users/ui/users.tsx
@@ -6,8 +6,10 @@ import { useQuery } from '@tanstack/react-query';
 import { UserApi, type User } from '@/entities/user';
 import { Button } from '@/shared/ui/button';
 import { ManageUserDialog } from '@/widgets/manage-user-dialog';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useSession } from '@/entities/session';
+
+type Filter = 'all' | 'students' | 'teachers' | 'admins';
 
 export function Users() {
   const users = useQuery({
@@ -17,7 +19,33 @@ export function Users() {
 
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState<Filter>('all');
+
   const session = useSession();
+
+  const filteredUsers = useMemo(() => {
+    if (!users.data) return [];
+
+    const query = searchQuery.toLowerCase();
+    const searchResults = users.data.filter(
+      (u) =>
+        u.surname.toLowerCase().includes(query) ||
+        u.name.toLowerCase().includes(query) ||
+        u.user_name.toLowerCase().includes(query),
+    );
+
+    switch (filter) {
+      case 'all':
+        return searchResults;
+      case 'students':
+        return searchResults.filter((u) => u.role === 'student');
+      case 'teachers':
+        return searchResults.filter((u) => u.role === 'teacher');
+      case 'admins':
+        return searchResults.filter((u) => u.role === 'admin');
+    }
+  }, [users.data, searchQuery, filter]);
 
   if (users.isPending) return null;
   if (users.error) return <span>{users.error.message}</span>;
@@ -25,9 +53,15 @@ export function Users() {
   return (
     <div>
       <div className={classes.filters}>
-        <Input className={classes.search} placeholder="Поиск по имени или ID" role="search" />
+        <Input
+          className={classes.search}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Поиск по имени или ID"
+          role="search"
+        />
 
-        <Select>
+        <Select value={filter} onChange={(e) => setFilter(e.target.value as Filter)}>
           <option value="all">Все роли</option>
           <option value="students">Студенты</option>
           <option value="teachers">Преподаватели</option>
@@ -53,7 +87,7 @@ export function Users() {
           </thead>
 
           <tbody>
-            {users.data.map((user) => (
+            {filteredUsers.map((user) => (
               <tr key={user.user_id} className={classes.tr}>
                 <td className={classes.td}>
                   <span>
