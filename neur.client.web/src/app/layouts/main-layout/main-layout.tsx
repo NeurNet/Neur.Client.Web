5cf07050c00f81a9579befe9244022813dded6c5
Kirill Siukhin <kirillsiukhin@proton.me>
2026-05-03 07:41:36 +0500

    feat: check user session in layout

--- neur.client.web/src/app/layouts/main-layout/main-layout.tsx
+++ neur.client.web/src/app/layouts/main-layout/main-layout.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from 'react-router';
+import { useSession } from '@/features/auth';
+import { Navigate, Outlet } from 'react-router';
 
 export function MainLayout() {
+  const session = useSession();
+
+  if (session.isPending) return null;
+  if (!session.data) return <Navigate to="/login" replace />;
+
   return (
     <div>
       <Outlet />
